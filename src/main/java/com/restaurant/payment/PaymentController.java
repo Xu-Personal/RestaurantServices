0a89215fd51e71536681fdbddaf5b518fb588890
Benjamin Xu <32624725+benj58xu@users.noreply.github.com>
2020-10-14 21:47:08 -0400

    Update PaymentController.java

--- src/main/java/com/restaurant/payment/PaymentController.java
+++ src/main/java/com/restaurant/payment/PaymentController.java
@@ -7,8 +7,7 @@ public class PaymentController {
 public ResponseEntity<Object> chargeByCreditCard(@RequestBody Charge charge) {
     // calls B2B service of the corresponding credit card company
     // persist the charge information in database
-    // returns the authorization ID
-    // Note: it is better to put
+    // returns the same Charge object with confirmation ID filled
 }
 
 }
