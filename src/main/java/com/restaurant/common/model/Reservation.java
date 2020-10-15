5032a8d2546db3791f109470f0b4b46271112572
Benjamin Xu <32624725+benj58xu@users.noreply.github.com>
2020-10-14 21:19:47 -0400

    Create Reservation.java

---
+++ src/main/java/com/restaurant/common/model/Reservation.java
@@ -0,0 +1,13 @@
+package com.restaurant.common.model;
+
+public class Reservation {
+
+  String restaurantId;
+  String tableId;
+  long time;
+  int duration;
+  String customerName;
+  String customerPhoneNumber;
+  int numberOfPeople;
+  
+}
