c20b55fa93d7539441b4d7b6d133e7819c46c493
Benjamin Xu <32624725+benj58xu@users.noreply.github.com>
2020-10-14 21:11:45 -0400

    Create CreditCardInfo.java

---
+++ src/main/java/com/restaurant/common/model/CreditCardInfo.java
@@ -0,0 +1,12 @@
+package com.restaurant.common.model;
+
+@Document
+public class CreditCardInfo {
+  String cardType;
+  String cardNumber;
+  String securityCode;
+  Long expirationDate;
+  String cardHolderName;
+  
+  
+}
