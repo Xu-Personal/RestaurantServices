af7bea3c9126793c98f0dd037625bb72e2068f21
Benjamin Xu <32624725+benj58xu@users.noreply.github.com>
2020-10-14 21:21:33 -0400

    Update MenuItem.java

--- src/main/java/com/restaurant/common/model/MenuItem.java
+++ src/main/java/com/restaurant/common/model/MenuItem.java
@@ -2,6 +2,9 @@ package com.restaurant.common.model;
 
 @Document
 public class MenuItem {
-
-
+    String restarantId;
+    String dishId;
+    String dishName;
+    String description;
+    float price;
 }
