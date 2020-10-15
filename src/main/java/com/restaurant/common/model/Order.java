635a277f9904f25b5aaf343aedc7283685043d2b
Benjamin Xu <32624725+benj58xu@users.noreply.github.com>
2020-10-14 21:28:15 -0400

    Update Order.java

--- src/main/java/com/restaurant/common/model/Order.java
+++ src/main/java/com/restaurant/common/model/Order.java
@@ -1,12 +1,15 @@
+package com.restaurant.common.model;
+
 @Document
 public class Order {
+  String restaurantId;
   String orderId;
   long orderTime;
-  DeliveryRequest deliveryRequest;
-  PaymentInfo paymentInfo;
+  Charge chargeInfo;
   OrderStatus status;
   long completeTime;
-  String tableId;
   OrderType orderType; // take out, delivery, inside seating, outside seating
+  Reservation reservation;    // this includes walk-in assignments (just-in-time reservation); only for dine-in
+  Delivery delivery;          // only for home delivery
   List<MenuItem> orderItems;
 }
