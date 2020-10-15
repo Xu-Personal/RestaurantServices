abb66e593d2babd49c3085fa903c09e0f0443eab
Benjamin Xu <32624725+benj58xu@users.noreply.github.com>
2020-10-14 21:43:11 -0400

    Update OrderController.java

--- src/main/java/com/restaurant/order/OrderController.java
+++ src/main/java/com/restaurant/order/OrderController.java
@@ -5,10 +5,14 @@ public class OrderController {
 
 @RequestMapping(value="/menu", method=GET)
 public ResponseEntity<Object> getMenus(@PathVariable String restaurantId) {
+  // return an array of MenuItem objects
 }
 
 @RequestMapping(value="/order", method=POST)
-public ResponseEntity<Object> placeOrder((@PathVariable String restaurantId, @RequestBody Order order) {
+public ResponseEntity<Object> placeOrder(@PathVariable String restaurantId, @RequestBody Order order) {
+  // depending on how the order is placed, the information for reservation and payment may not be
+  // filled yet
+  // returns the same Order object with additional data such as Reservation and Delivery populated
 }
 
 }
