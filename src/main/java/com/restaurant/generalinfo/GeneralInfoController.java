9cf6c6dfe554bca633d1fb032d650ef72921503d
Benjamin Xu <32624725+benj58xu@users.noreply.github.com>
2020-10-14 21:40:03 -0400

    Update GeneralInfoController.java

--- src/main/java/com/restaurant/generalinfo/GeneralInfoController.java
+++ src/main/java/com/restaurant/generalinfo/GeneralInfoController.java
@@ -6,12 +6,13 @@ public ResponseEntity<Object> getRestaurantInfo(@PathVariable String restaurantI
    // If not cached, load it from database and put in the cache
    // If ETag is sent in the request, check the last update time of the restaurant
    //   and return HTTP 304 if there is no change 
-   // Return HTTP 200 with the data
+   // Return HTTP 200 with the data Restaurant
 }
 
 @RequestMapping(value="/menu/{restaurantId}", method=GET)
 public ResponseEntity<Object> getMenu(@PathVariable String restaurantId) {
   // Invoke services provided by com.restaurant.order.getMenu and return the result.
   // This permits the external clients with a single endpoint to use
+  // Returns an array of MenuItem entries
 }
 
