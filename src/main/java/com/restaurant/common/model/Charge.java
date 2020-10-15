38a274f138c354c816304265ba69689f8de6be97
Benjamin Xu <32624725+benj58xu@users.noreply.github.com>
2020-10-14 21:22:42 -0400

    Update Charge.java

--- src/main/java/com/restaurant/common/model/Charge.java
+++ src/main/java/com/restaurant/common/model/Charge.java
@@ -1,9 +1,11 @@
 package com.restaurant.common.model;
 
+@Document
 public class Charge {
-
+  String restaurantId;
   float amount;
   CreditCardInfo creditCard;
+  long time;
   String confirmationNumber;
   
 }
