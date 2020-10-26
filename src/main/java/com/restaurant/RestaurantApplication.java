cbb1195935fbd69d779ddbc69ac70d827d4e48d9
Benjamin Xu <benjxu@yahoo.com>
2020-10-26 08:40:30 -0400

    another update

--- src/main/java/com/restaurant/RestaurantApplication.java
+++ src/main/java/com/restaurant/RestaurantApplication.java
@@ -1,7 +1,9 @@
 package com.restaurant;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class RestaurantApplication {
 
     public static void main(String[] args) {
